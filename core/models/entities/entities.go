d1efbc642dea4f6d557c50c63d39a7bd0985d2b3
TangXuanzhao <xuanzhaotang@gmail.com>
2018-03-26 14:25:53 +0800

    add find project method

--- core/models/entities/entities.go
+++ core/models/entities/entities.go
@@ -2,6 +2,10 @@ package entities
 
 // ProjectInfo store project information
 type ProjectInfo struct {
+	ProjectID   int `xorm:"pk autoincr 'project_id'"`
+	ProjectName string
+	UserID      int `xorm:"'user_id'"`
+	Language    int
 }
 
 func (u ProjectInfo) TableName() string {
--- core/models/service/service.go
+++ core/models/service/service.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"github.com/txzdream/websocket-echo-server_end/core/models/entities"
+)
+
+func NewProject(projectName string, language int) entities.ProjectInfo {
+	// Get username
+	userID := GetUserID()
+	project := entities.ProjectInfo{
+		ProjectName: projectName,
+		UserID: userID,
+		Language: language,
+	}
+	return project
+}
+
+// CreateProject insert a new project to the db
+func CreateProject(project entities.ProjectInfo) (bool, error) {
+	_, err := entities.Engine.InsertOne(project)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func GetProjects(userID int) []entities.ProjectInfo {
+	projects := make([]entities.ProjectInfo, 0)
+	entities.Engine.Where("projects.user_id = ?", userID).Find(&projects)
+	return projects
+}
+
+func GetUserID() int {
+	// TODO: return actual user id
+	return 1
+}
